f4ec4e0d59a23d0189b0773fe6d717a76352c3be
Little Mike <code@littlemike.co.uk>
2017-03-14 21:09:17 +0000

    Add *actual* initialization of repositories

--- src/test/kotlin/uk/co/littlemike/gishgraph/ddag/git/GitDDAGInitialisationTest.kt
+++ src/test/kotlin/uk/co/littlemike/gishgraph/ddag/git/GitDDAGInitialisationTest.kt
@@ -4,30 +4,38 @@ import com.winterbe.expekt.should
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.given
 import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.api.dsl.on
+import org.jetbrains.spek.api.dsl.xgiven
 import java.nio.file.Files
 import java.nio.file.Path
 
 object GitDDAGInitialisationTest : Spek({
     var workingDirectory : Path
 
-    given("The repository is initialised in a new directory") {
+    given("The working directory does not exist") {
         workingDirectory = Files.createTempDirectory("GitDGraphTest-").resolve("repo")
 
-        beforeGroup {
+        on("Initialisation of the repo") {
             GitDDAG(workingDirectory)
         }
 
-        it("Creates a new folder for the working directory") {
-            workingDirectory.toFile().isDirectory.should.equal(true)
+        it("Initializes in the new working directory $workingDirectory") {
+            workingDirectory.resolve(".git").toFile().isDirectory.should.equal(true)
+            GitRepository(workingDirectory).isInitialized().should.equal(true)
         }
     }
 
-    given("The repository is initialised in a directory that already exists") {
-        workingDirectory = Files.createTempDirectory("GitDGraphTest").resolve("repo")
-        GitDDAG(workingDirectory)
+    given("The working directory exists") {
+        workingDirectory = Files.createTempDirectory("GitDGraphTest-").resolve("repo")
+        GitRepository(workingDirectory).init()
 
-        it("Does not fail") {
-            workingDirectory.toFile().isDirectory.should.equal(true)
+        on("Initialisation of the repo") {
+            GitDDAG(workingDirectory)
+        }
+
+        it("Initializes in the existing working directory") {
+            workingDirectory.resolve(".git").toFile().isDirectory.should.equal(true)
+            GitRepository(workingDirectory).isInitialized().should.equal(true)
         }
     }
 })
--- src/test/kotlin/uk/co/littlemike/gishgraph/ddag/git/GitRepository.kt
+++ src/test/kotlin/uk/co/littlemike/gishgraph/ddag/git/GitRepository.kt
@@ -2,10 +2,15 @@ package uk.co.littlemike.gishgraph.ddag.git
 
 import org.eclipse.jgit.api.Git
 import org.eclipse.jgit.internal.storage.file.FileRepository
-import java.io.File
+import org.eclipse.jgit.lib.Constants
 import java.nio.file.Path
 
-
-class GitDDAG(workingDirectory: Path) {
+class GitRepository(workingDirectory: Path) {
     val git = Git.init().setDirectory(workingDirectory.toFile()).call()
+
+    fun isInitialized() = git.status().call().isClean
+
+    fun init() {
+        // Do nothing, it is always initialized by JGit
+    }
 }
